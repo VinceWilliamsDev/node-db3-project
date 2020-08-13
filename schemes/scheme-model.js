b9c0ee432e95974a899b772a92879a9d4754b49e
Vince Williams <williamsvm20@gmail.com>
2020-08-13 11:12:51 -0400

    find and findById are working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -13,3 +13,31 @@ module.exports = {
 function find() {
     return db('schemes')
 }
+
+function findById(id) {
+    return db('schemes')
+        .where('id', id)
+        .then(scheme => {
+            if (scheme.length < 1) {
+                return null
+            } else {
+                return scheme[0]
+            }
+        })
+}
+
+function findSteps(id) {
+    return null
+}
+
+function add(scheme) {
+    return null
+}
+
+function update(changes, id) {
+    return null
+}
+
+function remove(id) {
+    return null
+}
